9f79c400ac7082c2ce166bb208d7c39df95e7327
Hamza-Senhaji-RHazi <hamza.senhaji-rhazi@epita.fr>
2017-07-04 19:26:54 +0200

    add myProducer and myConsumer classes

---
+++ src/main/scala/utils/myProducer.scala
@@ -0,0 +1,28 @@
+package utils
+
+import java.util.{Date, Properties}
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.scalatest.prop
+
+import scala.util.Random
+
+/**
+  * Created by Hamza on 04/07/2017.
+  */
+object myProducer {
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  val localhost = "10.41.170.143"
+  props.put("zookeeper.connect", localhost+ ":2181")
+  props.put("client.id", "ScalaProducerExample")
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    def sendmsg(msg:String, ip:String, topic:String):Unit =
+    {
+      val producer = new KafkaProducer[String, String](props)
+      val data = new ProducerRecord[String, String](topic, ip, msg)
+      producer.send(data)
+      producer.close()
+    }
+}
